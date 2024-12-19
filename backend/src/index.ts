e12983f293d05303a3044b7e89bc7889fc32c692
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-12-19 19:06:28 +0200

    add messages frontend functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
+const PORT  = process.env.PORT || 5000;
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
@@ -12,6 +13,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ frontend/src/hooks/useGetMessages.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      if (!selectedConversation) return;
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation.id}`);
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setMessages(data);
+      } catch (error: any) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getMessages();
+  }, [selectedConversation, setMessages]);
+  return { loading, messages };
+};
+
+export default useGetMessages;
