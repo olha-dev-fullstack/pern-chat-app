5c9dc2dbb0a8f7448d321c6be9571434daf58498
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-12-19 13:44:30 +0200

    finish messages routes;

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import protectedRoute from "../middleware/protectedRoute";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller";
 
 const router = express.Router();
 
-router.get("/conversations", (req, res) => {
-  res.send("Conversation route");
-});
+router.get("/conversations", protectedRoute, getUsersForSidebar);
+router.get("/:id", protectedRoute, getMessages);
+router.post("/send/:id", protectedRoute, sendMessage);
 
 export default router;
