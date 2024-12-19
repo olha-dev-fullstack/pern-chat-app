695942875b2fbab68d2e740d00e1163e30767749
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-12-19 17:25:52 +0200

    finish auth

---
+++ frontend/src/hooks/useLogin.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
+
+type LoginInputs = {
+  username: string;
+  password: string;
+};
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async (inputs: LoginInputs) => {
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.error);
+      setAuthUser(data);
+    } catch (error: any) {
+      console.log(error.message)
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, login };
+};
+
+export default useLogin;
